fe739ea43cdaba8824d371f509e9dd09bd66a660
123 <123@DESKTOP-U5B092S>
2021-07-15 20:42:26 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import Element from 'element-ui'
+import "element-ui/lib/theme-chalk/index.css"
+import axios from 'axios'
+import "./axios"
+import 'mavon-editor/dist/css/index.css'
+import mavonEditor from 'mavon-editor'
+import "./permission"
+Vue.use(Element)
+Vue.use(mavonEditor)
 Vue.config.productionTip = false
+Vue.prototype.$axios = axios
 
 new Vue({
   router,
--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    token: '',
+    suerInfo: JSON.parse(sessionStorage.getItem("userInfo"))
   },
   mutations: {
+    SET_TOKEN: (state, token) => {
+      state.token = token
+      localStorage.setItem("token", token)
+    },
+    SET_USERINFO: (state, userInfo) => {
+      state.userInfo = userInfo
+      sessionStorage.setItem("userInfo", JSON.stringify(userInfo))
+    },
+    REMOVE_INFO: (state) => {
+      state.token = ''
+      state.userInfo = {}
+      localStorage.setItem("token", '')
+      sessionStorage.setItem("userInfo", JSON.stringify(''))
+    }
+  },
+  getters: {
+    // get
+    getUser: state => {
+      return state.userInfo
+    }
+
   },
   actions: {
+    getUser: state => {
+      return state.userInfo
+    }
   },
   modules: {
   }
